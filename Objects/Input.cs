b66ec7f70aa428eb836406e5771a74eedfff051f
Bryan Kruman <bryankruman@gmail.com>
2024-12-06 08:55:11 -0500

    Quick readonly change

--- Objects/Input.cs
+++ Objects/Input.cs
@@ -3,6 +3,6 @@ namespace Revi;
 public class Input(string label, string text)
 {
     public readonly string Identifier = Util.Identifierize(label);
-    public string Label = label;
-    public string Text = text;
+    public readonly string Label = label;
+    public readonly string Text = text;
 }
