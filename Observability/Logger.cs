be66b33bb4327ffb1c378811c6bd30ecb5235c3f
Bryan Kruman <bryankruman@gmail.com>
2024-12-19 19:30:11 -0500

    Add pseudo code in comment

--- Observability/Logger.cs
+++ Observability/Logger.cs
@@ -15,5 +15,26 @@ public static class Logger
         Console.WriteLine(message);
         return node;
     }
-    
+ 
+    /*
+         private static LogNode Log(
+           LogNode? parent, 
+           string? identifier, 
+           string? message = null,
+           string? tags = null,
+           int? cycle = null,
+           object? o1 = null, 
+           object? o2 = null)
+       {
+           return Logger.LogNode(
+               parent, 
+               identifier, 
+               message,
+               tags,
+               cycle,
+               o1, 
+               o2);
+       }
+       //Log(null, "foobar", "log message", o1: tlds);
+     */
 }
